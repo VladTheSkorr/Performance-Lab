1e9268ac0c814495d6f6b025f6b5180449338349
VladTheSkorr <52859973+VladTheSkorr@users.noreply.github.com>
2019-08-03 20:53:11 +0300

    Checked

--- task1/task1.py
+++ task1/task1.py
@@ -1,7 +1,20 @@
+import sys
 import math
 import numpy as np
+import argparse as argp
 
-skorr = [float(i) for i in input().split()]
+#https://ergoz.ru/razbor-parametrov-komandnoy-stroki-v-python/
+def createParser():
+    parser = argp.ArgumentParser()
+    parser.add_argument('-n', '--name', required=True)
+    return parser
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+#http://qaru.site/questions/952/how-do-i-read-a-file-line-by-line-into-a-list
+skorr = [float(x.rstrip()) for x in open(namespace.name)]
 new = sorted(skorr)
 
 def mediana(skorr):
@@ -9,7 +22,7 @@ def mediana(skorr):
     if len(new) % 2 != 0:
         return new[l]
     else:
-        return (new[l] + new[l - 1]) / 2.0
+       return (new[l] + new[l - 1]) / 2.0
 print(np.percentile(new, 90))
 print(mediana(skorr))
 print(max(new))
